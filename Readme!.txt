edfab9baf640cd83d4cbb6ad3687d13284a04c17
tommyhawk95 <tommygaool@gmail.com>
2018-10-02 02:01:12 +0700

    Initial commit

---
+++ Readme!.txt
@@ -0,0 +1,8 @@
+This is an example for Computer Store.
+This is a free source code for basic HTML to Learning.
+Do not remove this!
+Thank You! :)
+
+
+Creator :
+--- Tommy Hawk ----
---
+++ contact.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Hutomo Khairinnas - Contact</title>
+<link href="stylesKWU.css" rel="stylesheet" type="text/css" />
+<link rel="shortcut icon" href="images/Hawk_logo.png" />
+</head>
+<body>
+<!-- begin #container -->
+<div id="container">
+	<!-- begin #header -->
+    <div id="header">
+    	<div class="logo"><a href="index.html"><img src="images/StoreLogo10.png"/></a></div>
+        <div class="menuContainer">
+          <div class="menu">
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="shop.html">Shop</a></li>
+                    <li><a href="about.html">About Us</a></li>
+                    <li id="active"><a href="contact.php">Contact Us</a></li>
+                </ul>
+          </div>
+                <ul class="icons">
+                    <li><a href="https://www.facebook.com/tommy.khairinnas"><img src="images/facebook.png" width="50" height="50" alt=""/></a></li>
+                    <li><a href="https://twitter.com/tommy_hawk93"><img src="images/Twitter.png" width="50" height="50" alt="" /></a></li>
+                </ul>
+            <div class="clearfloat"></div>
+        </div></div><br /><hr /><br />
+        <div align="center" style="">
+							<div id="map-canvas">
+                         <script src='https://maps.googleapis.com/maps/api/js?v=3.exp'></script><div style='overflow:hidden;height:245px;width:400px;'><div id='gmap_canvas' style='height:245px;width:400px;'></div><style>#gmap_canvas img{max-width:none!important;background:none!important}</style></div> <a href='https://mapswebsite.net/'>embedded google maps</a> <script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=a00fd4dff45381fe71e7cdf005a8247d20d73407'></script><script type='text/javascript'>function init_map(){var myOptions = {zoom:15,center:new google.maps.LatLng(0.6966818,101.56798509999999),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(0.6966818,101.56798509999999)});infowindow = new google.maps.InfoWindow({content:'<strong><b>Hawk Store</b></strong><br>JL Raya minas perawang <br>28772 Riau<br>'});google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);</script>
+                            </div>
+						</div>
+                        <br />
+                        <hr />
+                        <br />
+<div align="center">
+<!-- Do not change the code! -->
+<a id="foxyform_embed_link_736671" href="http://www.foxyform.com/">foxyform</a>
+<script type="text/javascript">
+(function(d, t){
+   var g = d.createElement(t),
+       s = d.getElementsByTagName(t)[0];
+   g.src = "http://www.foxyform.com/js.php?id=736671&sec_hash=9c6d27daab4&width=350px";
+   s.parentNode.insertBefore(g, s);
+}(document, "script"));
+</script>
+<!-- Do not change the code! -->
+</div><hr />
+    <!-- end #header -->
+<div id="footer" align="center">
+<div style="position:;left:27px;top:51px;width:288px;height:110px;text-align:center;z-index:37;">
+<span style="color:#808080;font-family:Arial;font-size:15px; float:center;"><strong>Links</strong></span>
+<div>
+   <div class="item" style="height:20px;text-align:left;"><span style="color:#696969;font-family:Arial;font-size:13px;"><u><a href="http://www.unikom.ac.id ">• www.unikom.ac.id <br></a></u></span></div>
+</div>
+<div style="clear:both">
+   <div class="item" style="height:18px;text-align:left;"><span style="color:#696969;font-family:Arial;font-size:13px;"><u><a href="http://kuliahonline.unikom.ac.id">• kuliahonline.unikom.ac.id</a></u></span></div>
+</div>
+<div style="clear:both">
+  <div class="item" style="height:20px;text-align:left;"><span style="color:#696969;font-family:Arial;font-size:13px;"><u><a href="http://blog.unikom.ac.id">• blog.unikom.ac.id <br></a></u></span></div>
+
+<div style="clear:both">
+   <div class="item" style="height:20px;text-align:left;"><span style="color:#696969;font-family:Arial;font-size:13px;"><u><a href="http://repository.unikom.ac.id">• repository.unikom.ac.id <br></a></u></span></div>
+</div>
+<div style="clear:both">
+   <div class="item" style="height:18px;text-align:left;"><span style="color:#696969;font-family:Arial;font-size:13px;"><u><a href="http://elib.unikom.ac.id">• elib.unikom.ac.id</a></u></span></div>
+</div>
+</div>
+</div>
+<br />
+<p>Designed by Hutomo Khairinnas © 2015</p> 
+</div>
+</div></div>
+    <!-- end #footer -->
+</body>
+</html>
